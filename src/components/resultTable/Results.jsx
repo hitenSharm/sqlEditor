59df0d2527dadc08838cea60de25c60cbdeaeb96
hitenSharma17 <hiten.sharma1708@gmail.com>
2023-11-25 18:18:53 +0530

    added download feature

--- src/components/resultTable/Results.jsx
+++ src/components/resultTable/Results.jsx
@@ -1,6 +1,7 @@
-import { Table } from "antd";
+import { Button, Table } from "antd";
 import { useEffect, useState } from "react";
 import { useAppContext } from "../../context/AppContext";
+import { CSVLink } from "react-csv";
 
 export const ResultsTable = () => {
   const { currentCode, fetchSQLData } = useAppContext();
@@ -10,7 +11,7 @@ export const ResultsTable = () => {
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    if (currentCode !== '--Write query here') {      
+    if (currentCode !== '--Write query here') {
       const fetchDataAndSetState = async () => {
         try {
           const { resultData, totalRowsInTable } = await fetchSQLData(currentCode);
@@ -34,24 +35,38 @@ export const ResultsTable = () => {
   // Assuming the first row in the CSV contains headers
   const columns = data.length > 0 ? data[0].map((header) => ({ dataIndex: header, title: header })) : [];
 
+  //for downloading...
+  const csvData = paginatedData.map((row) =>
+    row.reduce((acc, value, index) => {
+      acc[data[0][index]] = value;
+      return acc;
+    }, {})
+  );
 
   return (
-    <Table
-      dataSource={paginatedData.slice(1).map((row, rowIndex) => {
-        // Mapped each row to an object with keys based on header names
-        const rowData = {};
-        row.forEach((value, columnIndex) => {
-          rowData[data[0][columnIndex]] = value;
-        });
-        return { ...rowData, key: rowIndex }; // Add a unique key to each row
-      })}
-      columns={columns}
-      pagination={{
-        current: currentPage,
-        total: totalRows,
-        defaultPageSize: 10, showSizeChanger: true, pageSizeOptions: ['10', '20', '30'],
-        onChange: handlePageChange,
-      }}
-    />
+    <>
+      {data.length > 0 ? <Button className=" mb-4">
+        <CSVLink data={csvData} filename="exported_data.csv">
+          Download as CSV
+        </CSVLink>
+      </Button> : <></>}
+      <Table
+        dataSource={paginatedData.slice(1).map((row, rowIndex) => {
+          // Mapped each row to an object with keys based on header names
+          const rowData = {};
+          row.forEach((value, columnIndex) => {
+            rowData[data[0][columnIndex]] = value;
+          });
+          return { ...rowData, key: rowIndex }; // Add a unique key to each row
+        })}
+        columns={columns}
+        pagination={{
+          current: currentPage,
+          total: totalRows,
+          defaultPageSize: 10, showSizeChanger: true, pageSizeOptions: ['10', '20', '30'],
+          onChange: handlePageChange,
+        }}
+      />
+    </>
   );
 }
